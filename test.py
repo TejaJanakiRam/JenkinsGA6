c70f5d1933e068439a09908bf4e6d3517f4df1a8
TejaJanakiRam <77498919+TejaJanakiRam@users.noreply.github.com>
2022-11-25 12:31:02 +0530

    Update and rename test2.py to test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import unittest
+
+from multi import multi
+
+class TestSum(unittest.TestCase):
+    def test1(self):
+        """
+        Test case to add two numbers
+        """
+        
+        result = multi(20,30)
+        self.assertEqual(result, 400)
+    def test2(self):
+        """
+        Test case to add two numbers
+        """
+        
+        result = multi(20,30)
+        self.assertEqual(result, 300)
+    def test3(self):
+        """
+        Test case to add two numbers
+        """
+        
+        result = multi(20,30)
+        self.assertEqual(result, 200)
+    def test4(self):
+        """
+        Test case to add two numbers
+        """
+        
+        result = multi(20,30)
+        self.assertEqual(result, 600)
+    def test5(self):
+        """
+        Test case to add two numbers
+        """
+        
+        result = multi(20,40)
+        self.assertEqual(result, 800)
+if __name__ == '__main__':
+    unittest.main()
