c6f8aaf2cb9319c780ce90b1ba7cd23ff98483c6
TejaJanakiRam <77498919+TejaJanakiRam@users.noreply.github.com>
2022-11-25 13:47:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,35 +7,35 @@ from multi import multi
 class TestSum(unittest.TestCase):
     def test1(self):
         """
-        Test case to add two numbers
+        Test case to multiply two numbers
         """
         
         result = multi(20,30)
         self.assertEqual(result, 400)
     def test2(self):
         """
-        Test case to add two numbers
+        Test case to multiply two numbers
         """
         
         result = multi(20,30)
         self.assertEqual(result, 300)
     def test3(self):
         """
-        Test case to add two numbers
+        Test case to muktiply two numbers
         """
         
         result = multi(20,30)
         self.assertEqual(result, 200)
     def test4(self):
         """
-        Test case to add two numbers
+        Test case to multiply two numbers
         """
         
         result = multi(20,30)
         self.assertEqual(result, 600)
     def test5(self):
         """
-        Test case to add two numbers
+        Test case to multiply two numbers
         """
         
         result = multi(20,40)
